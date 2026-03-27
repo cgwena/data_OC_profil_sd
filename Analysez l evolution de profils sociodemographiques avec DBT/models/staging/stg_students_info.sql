43d80263d0301d67211b0e1b45dd4a1ac19af1e2
Gwenaelle Bousses <cgwenaelle@hotmail.com>
2026-03-27 13:46:20 +0000

    new sources, staging for new sources, documentation

--- Analysez l evolution de profils sociodemographiques avec DBT/models/staging/stg_students_info.sql
+++
@@ -1,8 +0,0 @@
-select 
-    user_id,
-    path_category_name,
-    age_group,
-    coalesce(gender, 'unknown') as gender,
-    region,
-    year_path_started
-from {{ source('data_analysis','students_info') }}
