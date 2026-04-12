b98618cd7c1fbe1e397db1f2989d16ad5fd1da8e
Gwenaelle Bousses <cgwenaelle@hotmail.com>
2026-04-12 18:12:01 +0000

    Ajout documentation et tests

--- analyse_profils/models/intermediate/int_insee_chomage_region.sql
+++ analyse_profils/models/intermediate/int_insee_chomage_region.sql
@@ -4,13 +4,16 @@ with stg_chomage_region as (
 
 unpivoted_data as (
     select 
-        code_region,
-        nom_region,
+        case 
+            when UPPER(nom_region) in ('MAYOTTE', 'GUADELOUPE', 'GUYANE', 'MARTINIQUE', 'LA REUNION', 'RÉUNION') then 'DROM'
+            when UPPER(nom_region) in ('GRAND EST', 'GRAND-EST') THEN 'GRAND EST'
+            else UPPER(nom_region)
+        end as nom_region,
         periode,
         taux_chomage_region
     from stg_chomage_region
     unpivot (
-        taux_chomage_region FOR periode IN (
+        taux_chomage_region for periode in (
             t1_2022, t2_2022, t3_2022, t4_2022,
             t1_2023, t2_2023, t3_2023, t4_2023,
             t1_2024, t2_2024, t3_2024, t4_2024,
@@ -20,13 +23,11 @@ unpivoted_data as (
 )
 
 select 
-    code_region,
     nom_region,
     right(periode, 4)::int as annee,
     round(AVG(taux_chomage_region), 2) as taux_chomage_region
 from unpivoted_data
 where annee >= 2022
-group by 
-    code_region,
+group by
     nom_region,
     annee
